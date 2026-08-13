1811124cf5c1af9824fcd5412daff72e65f339a6
cayo-diebe <38913705+cayo-diebe@users.noreply.github.com>
2026-08-13 00:55:21 -0300

    Publish site content from admin (13/08/2026, 03:55:21)

--- published-data.js
+++ published-data.js
@@ -1,5 +1,5 @@
 window.ND_PUBLISHED_DATA = {
-  "exportedAt": "2026-08-13T03:53:46.027Z",
+  "exportedAt": "2026-08-13T03:55:19.483Z",
   "siteSettings": {
     "homeHeroImage": "assets/nicholas-dieter-nevoa.png",
     "instagramUrl": "https://www.instagram.com/nicholas.dieter/",
@@ -726,7 +726,7 @@ window.ND_PUBLISHED_DATA = {
             "photo": "assets/uploads/2026-08-13/2202be2154f9a2ab8d5fd1e3.jpg"
           },
           "nextClass": {
-            "dates": "Começa dia 9 de setembro",
+            "dates": "Começa dia 9 de setembro de 2026",
             "schedule": "Quartas feiras, das 19 às 22h",
             "workload": "12 horas mensais",
             "location": "Presencial: no Teatro Escola Mario Persico",
